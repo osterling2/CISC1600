ee368adb66c28ea49c1d15dbafe8caebdfd9b18a
osterling2 <osterling2@fordham.edu>
2016-02-22 15:07:22 -0500

    Create loop.cc

---
+++ loop.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+        int sum=0;
+        for( int i=1; 1<=3; i++)
+
+        {
+                int x
+                cin >> x;
+                sum = sum + x;
+        }
+        cout << " sum =" << sum << endl;
+        return 0;
+}
