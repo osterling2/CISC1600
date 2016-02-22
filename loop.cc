9abeb0d6d18cfedc5733bd0dcaddc57e3517a7d3
osterling2 <osterling2@fordham.edu>
2016-02-22 16:10:40 -0500

    Update loop.cc

--- loop.cc
+++ loop.cc
@@ -4,12 +4,12 @@ using namespace std;
 int main()
 {
         int sum=0;
-        for( int i=1; 1<=3; i++)
+        for( int A=1; 1<=3; A++)
 
         {
-                int x
-                cin >> x;
-                sum = sum + x;
+                int z
+                cin >> z;
+                sum = sum + z;
         }
         cout << " sum =" << sum << endl;
         return 0;
